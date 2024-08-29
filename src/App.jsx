04c73c6c69e61da03978d719bfa4cb880f59b7af
rafael valbuena <artillero.satanas@gmail.com>
2024-08-29 16:34:10 -0500

    organizado como componente individual

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,10 @@
-import React, { useRef, useState } from 'react';
-import styles from './App.module.css';
+import React from 'react'
+import { Slider } from './components/Slider/Slider'
 
 export const App = () => {
-  const [currentSlide, setCurrentSlide] = useState(1);
-  const cubeRef = useRef(null);
-  const nextButtonRef = useRef(null);
-  const prevButtonRef = useRef(null);
-
-  const handleNextClick = () => {
-    cubeRef.current.classList.add(styles.animate);
-    setCurrentSlide( currentSlide - 1);
-    setTimeout(() => {
-      cubeRef.current.classList.remove(styles.animate);
-    }, 300);
-  };
-
-  const handlePrevClick = () => {
-    cubeRef.current.classList.add(styles.animateInv);
-    setCurrentSlide( currentSlide + 1);
-    setTimeout(() => {
-      cubeRef.current.classList.remove(styles.animateInv);
-    }, 300);
-  };
-
-  const cubeStyles = {
-    transform: `rotateY(${(currentSlide - 1) * 90}deg)`,
-  };
-
   return (
-    <div className={styles.carousel}>
-      <div className={styles.cube} ref={cubeRef} style={cubeStyles}>
-        <div className={styles.slide}>Slide 1</div>
-        <div className={styles.slide}>Slide 2</div>
-        <div className={styles.slide}>Slide 3</div>
-        <div className={styles.slide}>Slide 4</div>
-      </div>
-      <button className={styles.next} ref={nextButtonRef} onClick={handleNextClick}>Next</button>
-      <button className={styles.prev} ref={prevButtonRef} onClick={handlePrevClick}>Prev</button>
+    <div>
+      <Slider/>
     </div>
-  );
+  )
 }
---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,42 @@
+import React, { useRef, useState } from 'react';
+import styles from './Slider.module.css';
+
+export const Slider = () => {
+      const [currentSlide, setCurrentSlide] = useState(1);
+      const cubeRef = useRef(null);
+      const nextButtonRef = useRef(null);
+      const prevButtonRef = useRef(null);
+    
+      const handleNextClick = () => {
+        cubeRef.current.classList.add(styles.animate);
+        setCurrentSlide( currentSlide - 1);
+        setTimeout(() => {
+          cubeRef.current.classList.remove(styles.animate);
+        }, 300);
+      };
+    
+      const handlePrevClick = () => {
+        cubeRef.current.classList.add(styles.animateInv);
+        setCurrentSlide( currentSlide + 1);
+        setTimeout(() => {
+          cubeRef.current.classList.remove(styles.animateInv);
+        }, 300);
+      };
+    
+      const cubeStyles = {
+        transform: `rotateY(${(currentSlide - 1) * 90}deg)`,
+      };
+    
+      return (
+        <div className={styles.carousel}>
+          <div className={styles.cube} ref={cubeRef} style={cubeStyles}>
+            <div className={styles.slide}>Slide 1</div>
+            <div className={styles.slide}>Slide 2</div>
+            <div className={styles.slide}>Slide 3</div>
+            <div className={styles.slide}>Slide 4</div>
+          </div>
+          <button className={styles.next} ref={nextButtonRef} onClick={handleNextClick}>Next</button>
+          <button className={styles.prev} ref={prevButtonRef} onClick={handlePrevClick}>Prev</button>
+        </div>
+      );
+}
